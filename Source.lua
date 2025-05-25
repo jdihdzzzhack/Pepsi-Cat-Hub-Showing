16330f4a9f71ebeda732377235669315bb515aef
Komaru_Scripter <Asanali220k@gmail.com>
2025-05-26 00:43:18 +0500

    Create Source.lua

---
+++ Source.lua
@@ -0,0 +1,23 @@
+if game.PlaceId == 4954096313 then
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Hello!";
+        Text = "Pls join in game";
+        Duration = 5;
+    })
+elseif game.PlaceId == 5096191125 then
+    loadstring(game:HttpGet("https://pastebin.com/raw/pP0QtSEC"))()
+elseif game.PlaceId == 5941839954 then
+loadstring(game:HttpGet("https://pastebin.com/raw/pP0QtSEC"))()
+elseif game.PlaceId == 3851622790 then
+loadstring(game:HttpGet("https://pastebin.com/raw/Zf5L6ch6"))()
+elseif game.PlaceId == 4620170611 then
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/8535ba5bb10efeb4cdebb69c22289eeb"))()
+elseif game.PlaceId == 14419907512 then
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/b5169f18eba859423ed4ecb59afbb4c5"))()
+else
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Hello!";
+        Text = "Game don't support";
+        Duration = 5;
+    })
+end
